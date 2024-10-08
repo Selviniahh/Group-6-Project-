6d80b673b6f570e4d24e3ee241f360c3d388264f
Selviniahh <selviniahh@gmail.com>
2024-10-08 17:22:48 -0400

    My test Commit to make sure collaboration is working fine

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"); //I am changing this already existing line
 
 app.Run();
+
+//This is extra two line that I am changing 
